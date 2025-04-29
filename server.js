900d59ef816375e96bab1baa68a65e3591c131cc
CristhianRivas <c.rivas@estudiantec.cr>
2025-04-28 18:08:49 -0600

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const userRoutes = require('./routes/userRoutes');
-const notificationRoutes = require('./routes/notificationRoutes');
+const matchRoutes = require('./routes/matchRoutes'); // Importar las rutas de partidos
 
 // Load environment variables
 dotenv.config();
